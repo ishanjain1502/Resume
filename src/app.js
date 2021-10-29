019c5e82aaad85b709219066e79676a6c7478eba
Ishan Jain <56102033+ishanjain1502@users.noreply.github.com>
2021-10-29 17:40:04 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -2,24 +2,13 @@ const express = require('express');
 const app = express();
 require('../src/db/conn');
 const path = require("path");
-const hbs = require('hbs');
 
 const port = process.env.PORT || 3000;
 
 
-// const view_path = path.join(__dirname, "../templates/views");
-const partials_path = path.join(__dirname, "../templates/partials");
 
 // console.log(path.join(view_path));
 
-
-app.set("view engine", "hbs");
-// app.use("views", view_path);
-hbs.registerPartials(partials_path);
-
-app.get('/', (req, res) => {
-    res.render("index");
-})
 
 app.listen(port, () => {
     console.log(`Connected on port ${port} `);
